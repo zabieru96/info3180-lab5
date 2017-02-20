10f8366dc1b1da35177139e469d2491917ceee95
Yannick Lyn Fatt <yannick.lynfatt@gmail.com>
2017-02-19 22:33:27 -0500

    initial commit

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "change this to be a more random key"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://user:password@localhost/database"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
+
+db = SQLAlchemy(app)
+
+# Flask-Login login manager
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+app.config.from_object(__name__)
+from app import views
