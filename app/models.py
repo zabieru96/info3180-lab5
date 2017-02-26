b63df045fdd208c09d6a2100a98412b8bd1b5a17
Zabieru <zabieru96@hotmail.com>
2017-02-26 04:07:07 +0000

    Added Password Field to Model

--- app/models.py
+++ app/models.py
@@ -5,6 +5,7 @@ class UserProfile(db.Model):
     first_name = db.Column(db.String(80))
     last_name = db.Column(db.String(80))
     username = db.Column(db.String(80), unique=True)
+    password = db.Column(db.String(255))
 
     def is_authenticated(self):
         return True
