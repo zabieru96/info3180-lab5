1f456978211572e829cec8f01b49d39d2f4cbc9b
Zabieru <zabieru96@hotmail.com>
2017-02-26 04:20:43 +0000

    Update the /login route

--- app/views.py
+++ app/views.py
@@ -29,22 +29,24 @@ def about():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
-    if request.method == "POST":
+    if request.method == "POST"  and form.validate_on_submit():
         # change this to actually validate the entire form submission
         # and not just one field
         if form.username.data:
             # Get the username and password values from the form.
-
+            username = form.username.data
+            password = form.password.data
             # using your model, query database for a user based on the username
             # and password submitted
             # store the result of that query to a `user` variable so it can be
             # passed to the login_user() method.
-
-            # get user id, load into session
-            login_user(user)
-
-            # remember to flash a message to the user
-            return redirect(url_for("home")) # they should be redirected to a secure-page route instead
+            user = UserProfile.query.filter_by(username=username, password=password).first()
+            if user is not None:
+                # get user id, load into session
+                login_user(user)
+                flash('Logged in successfully.', 'success')
+                # remember to flash a message to the user
+                return redirect(url_for("secure-page")) # they should be redirected to a secure-page route instead
     return render_template("login.html", form=form)
 
 # user_loader callback. This callback is used to reload the user object from
