9c054660345afae9c6ed0efbb98461e53e6577d1
Zabieru <zabieru96@hotmail.com>
2017-02-26 04:05:00 +0000

    Created Postgres DB and Migrations

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,8 @@ from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "change this to be a more random key"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://user:password@localhost/database"
+app.config['SECRET_KEY'] = "wdafegrssth6454#$$@RQ#T$Et44323##"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://lab5:password123@localhost/lab5"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 
 db = SQLAlchemy(app)
